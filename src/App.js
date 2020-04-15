ec1d7cad4606b09f8e35fd534541e043969b085f
Robin <mehmet.ucar@imediapro.co.uk>
2020-04-15 13:54:29 +0100

    server side successfully done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder'
 
 class App extends Component{
+
+
   render(){
     return(
       <div>
 
       <Layout>
-        <BurgerBuilder></BurgerBuilder>
+        <BurgerBuilder />
       </Layout>
 
       </div>
