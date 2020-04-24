bdc687dbf48e09fd5fdc87f853cbfaf6bdc70ca0
Robin <mehmet.ucar@imediapro.co.uk>
2020-04-24 14:27:41 +0100

    navigating to checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
+        console.log(this.props)
         axios.get('https://react-my-burger-e3851.firebaseio.com/ingredients.json')
             .then( response => {
                 this.setState( { ingredients: response.data } );
@@ -92,7 +93,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState( { loading: true } );
+        /* this.setState( { loading: true } );
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -115,7 +116,8 @@ class BurgerBuilder extends Component {
             } )
             .catch( error => {
                 this.setState( { loading: false, purchasing: false } );
-            } );
+            } ); */
+            this.props.history.push('/checkout');
     }
 
     render () {
