0e271a56fd752ae239a574b4239331a0252914e5
Robin <mehmet.ucar@imediapro.co.uk>
2020-04-25 23:11:16 +0100

    addding dropdown

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -9,13 +9,26 @@ const Input = (props) => {
       className={classes.InputElement}
       {...props.elementConfig}
       value = {props.value} />;
-      break;
+    break;
     case ('textarea'):
       inputElement = <textarea
       className={classes.InputElement}
       {...props.elementConfig}
       value = {props.value} />;
-      break;
+    break;
+    case ('select'):
+      inputElement = (
+        <select
+            className={classes.InputElement}
+            value = {props.value}>
+            {props.elementConfig.options.map(option => (
+              <option key={option.value} value={option.value}>
+              {option.displayValue}
+              </option>
+            ))}
+        </select>
+      );
+    break;
     default:
       inputElement = <input
       className={classes.InputElement}
