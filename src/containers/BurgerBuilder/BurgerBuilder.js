d81b1f896a9675e6d896c54136a22a9aa55e29eb
Robin <mehmet.ucar@imediapro.co.uk>
2020-04-30 00:35:28 +0100

    adjusting checkout and contact data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,7 @@ class BurgerBuilder extends Component {
     // }
     state = {
 
-        purchasable: false,
+
         purchasing: false,
         loading: false,
         error: false
@@ -44,7 +44,7 @@ class BurgerBuilder extends Component {
             .reduce( ( sum, el ) => {
                 return sum + el;
             }, 0 );
-        this.setState( { purchasable: sum > 0 } );
+        return sum > 0 ;
     }
 
     purchaseHandler = () => {
@@ -57,16 +57,10 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
 
-            const queryParams = [];
-            for(let i in this.state.ingredients) {
-                queryParams.push(encodeURIComponent(i) + '= ' + encodeURIComponent(this.state.ingredients[i]));
-            }
-            queryParams.push('price= ' + this.state.totalPrice);
-            const queryString = queryParams.join('&');
-            this.props.history.push({
-                pathname: '/checkout',
-                search: '?' + queryString
-            });
+            this.props.history.push('/checkout')
+
+
+
     }
 
     render () {
@@ -87,7 +81,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                         price={this.props.price} />
                 </Aux>
