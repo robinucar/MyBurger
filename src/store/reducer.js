4f68a690343b53fbe03cba0f59d6d266a1e040c7
Robin <mehmet.ucar@imediapro.co.uk>
2020-04-29 23:48:01 +0100

    ingredients and total price states set up by redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,13 @@ const initalState = {
   totalPrice: 4,
 };
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
+
 const reducer = (state = initalState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
@@ -17,21 +24,21 @@ const reducer = (state = initalState, action) => {
         ...state,
         ingredients: {
           ...state.ingredients,
-          [action.ingredientName]: state.ingredients[ingredientName] + 1
-        }
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        },
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
         ...state,
         ingredients: {
           ...state.ingredients,
-          [action.ingredientName]: state.ingredients[ingredientName] - 1
-        }
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
       };
       default:
-        return {
-
-        }
+        return state
   }
 };
 
