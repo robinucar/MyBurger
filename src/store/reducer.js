712608a86448a8e4d4c2f6ba4649cc3fa842abf3
Robin <mehmet.ucar@imediapro.co.uk>
2020-04-29 14:50:19 +0100

    redux set up

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './action';
+
+const initalState = {
+  ingredients: null,
+        totalPrice: 4,
+};
+
+const reducer = (state = initalState, action) => {
+
+};
+
+export default reducer;
