0992a01b30046c2bc846e472a9a3d7237f892413
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-04 15:44:34 +0100

    created action creators

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as actionTypes from '../../store/action';
+import * as burgerBuilderActions from '../../store/actions/index';
 
 
 
@@ -117,8 +117,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded :(ingName) => dispatch({type:actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-        onIngredientRemoved :(ingName) => dispatch({type:actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+        onIngredientAdded :(ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
+        onIngredientRemoved :(ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
     }
 }
 
