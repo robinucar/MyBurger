08caaf4b748e2b9e85e020cc0e71484a50b0c7bb
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-07 16:35:04 +0100

    getting a token from backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import {connect} from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
