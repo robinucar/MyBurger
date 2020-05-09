1eeaba927243856f30888541ad4828f69d0eb838
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-09 15:12:15 +0100

    Redirecting the User to the Checkout Page

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -17,7 +17,8 @@ const authSuccess = (state, action) => {
     token: action.idToken,
     userId: action.userId,
     error: null,
-    loading: false
+    loading: false,
+    authRedirectPath: '/'
   })
 };
 
@@ -35,12 +36,17 @@ const authLogout = (state, action) => {
   })
 }
 
+const setAuthRedirectPath = (state, action) => {
+  return updateObject(state, {authRedirectPath: action.path})
+}
+
 const reducer = (state = initalState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START : return authStart(state, action);
     case actionTypes.AUTH_SUCCESS : return authSuccess(state, action);
     case actionTypes.AUTH_FAIL : return authFail(state,action);
     case actionTypes.AUTH_LOGOUT : return authLogout(state,action);
+    case actionTypes.SET_AUTH_REDIRECT_PATH: return setAuthRedirectPath(state,action)
 
     default:
       return state;
