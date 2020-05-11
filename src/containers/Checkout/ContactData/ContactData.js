00b25fd29376cdb688c02bd3d4f125edb3a720dd
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-11 15:24:48 +0100

    shared validatity methot drom utilty to auth and contactData components

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,7 +6,7 @@ import classes from '../ContactData/ContactData.module.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
-import { updateObject} from '../../../shared/utility';
+import { updateObject, checkValidity} from '../../../shared/utility';
 import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
@@ -138,38 +138,13 @@ class ContactData extends Component {
 
   }
 
-  checkValidity (value, rules) {
-    let isValid = true;
-    if(rules.required){
-      isValid = value.trim() !== '' && isValid;
-    }
 
-    if(rules.minLength){
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if(rules.maxLength){
-      isValid = value.length <= rules.maxLength && isValid
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid
-    }
-
-    return isValid;
-  }
 
   inputChangeHandler = (event, inputIdenfitier) => {
 
     const updatedOrderFormElement = updateObject(this.state.orderForm[inputIdenfitier], {
       value: event.target.value,
-      valid: this.checkValidity( event.target.value, this.state.orderForm[inputIdenfitier].validation),
+      valid: checkValidity( event.target.value, this.state.orderForm[inputIdenfitier].validation),
       touched: true,
     });
 
