97294888190523646dfc54e2c7291f66730c98db
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-11 13:24:32 +0100

    fixed checkout summary style and redirect to the front page and added click method to the sidedrawer component.

--- src/components/Navbar/SideDrawer/SideDrawer.js
+++ src/components/Navbar/SideDrawer/SideDrawer.js
@@ -19,7 +19,7 @@ const sideDrawer = ( props ) => {
                     <Logo />
                 </div>
                 <nav>
-                    <NavigationItems isAuthenticated={props.isAuth} />
+                    <NavigationItems isAuthenticated={props.isAuth} onClick={props.closed} />
                 </nav>
             </div>
         </Aux>
