b1c93ea027a7101b441517b4709ac966eccac09c
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-12 21:13:37 +0100

    BurgerBuilder container tested

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import * as actions from '../../store/actions/index';
 
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
     //     this.state = {...}
